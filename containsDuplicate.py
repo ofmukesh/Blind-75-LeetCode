ae932a18d2215814705b97eaf9daec52674f5523
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-03-03 20:14:20 +0530

    217. Contains Duplicate

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,3 +1,18 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums)!=len(set(nums))
+        hash_set={}
+
+        for ele in nums:
+            if hash_set.get(ele)==None:
+                hash_set[ele]=1
+            else:
+                return True
+        return False;
+    
+    
+#_Explain
+#create a empty hash_set
+#pick up each elements of array one by one & check :-
+#   if element exist in hash_set -> return True
+#   and if element not exist in hash_set -> add element to hash_set
+#   if no one element is same then return False
