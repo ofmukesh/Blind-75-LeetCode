36a8f05e8bb7ca889bcedd742f33f406ed2e9d8c
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-03-05 11:39:22 +0530

    152. Maximum Product Subarray

--- maxProduct.py
+++ maxProduct.py
@@ -1,8 +1,24 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        B = nums[::-1]
+        # using two variables to store max & min product of current subarray
+        min_p,max_p=1,1
         
-        for i in range(1, len(nums)):
-            nums[i] *= nums[i - 1] or 1
-            B[i] *= B[i - 1] or 1
-        return max(nums + B)
+        # use 3rd variable tostore maxium product of subarrays
+        res=max(nums)
+
+        for n in nums:
+            
+            # if current element is 0 then start with a new subarray
+            if n==0:
+                min_p=1
+                max_p=1
+                
+            # now updating max_p and min_p with min & max of ele, ele * min_p , ele * max_p
+            t=max_p
+            max_p=max(n,n*max_p,n*min_p)
+            min_p=min(n,n*t,n*min_p)
+            
+            # updating max product of subarrays
+            res=max(min_p,max_p,res)
+        return res
+   
