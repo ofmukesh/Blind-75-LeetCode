741eb59d341492ce4a74167207a21b801d73bb02
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-03-12 13:36:42 +0530

    268. Missing Number

--- missingNumber.py
+++ missingNumber.py
@@ -1,5 +1,10 @@
+
+#-------------------------Explainaion-------------------------
+# get the sum of the elements 0 to n & the sum of given array 
+# the ans is sum(0 to n) - sum(given array)
+
+#-------------------------Code------------------------
 class Solution:
-    #my solution
     def missingNumber(self, nums: List[int]) -> int:
         c_sum=0;
         m_sum=0;
@@ -7,11 +12,11 @@ class Solution:
             c_sum+=nums[i];
             m_sum+=i+1;
         return m_sum-c_sum;
-      
-      
-    #others solution
-#     def missingNumber(self, nums: List[int]) -> int:
-#         res=0;
-#         for i in range(len(nums)):
-#             res=res+i+1-nums[i];
-#         return res;
+    
+# same algo we will do like this
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res=len(nums);
+        for i in range(len(nums)):
+            res+=i-nums[i];
+        return res;
