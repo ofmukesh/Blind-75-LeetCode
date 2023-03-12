daa4f6251539c16a26a798886398d0ab3f109f48
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-03-12 17:27:09 +0530

    190. Reverse Bits

--- reverseBits.py
+++ reverseBits.py
@@ -3,8 +3,8 @@ class Solution:
         res = 0
         
         for i in range(32):
-            if n % 2 == 1:
-                res += 2 ** (32-i-1)
-            n = n >> 1
-        
+            lsb = n & 1 # getting the least significant bit
+            rlsb = lsb << (31-i) 
+            res= res | rlsb
+            n = n >> 1 # right shifting n by 1
         return (res)
