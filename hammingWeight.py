f24c68473d843cf36fbadc7b21caf507879bb776
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-03-12 13:04:06 +0530

    191. Number of 1 Bits

--- hammingWeight.py
+++ hammingWeight.py
@@ -2,12 +2,12 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         res = 0
 
-        #2 We will remove 1 using AND operator and count how many times 1`s removed and return it
+        #1 by using & bit operator, we will simply remove 1's one by one
         while n:
             n &= (n-1)
             res += 1
 
-        #2 Basically we were doing here is cheking last bit is 0 or 1 then right shifting given integer
+        #2 Basically we were doing here is cheking last bit is 0 or 1 then shifting right it
         # while n:
         #     res += (n % 2)
         #     n=n>>1
