1130853b273cadb1aabfcaff91d78394b78eb783
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2023-03-12 13:05:00 +0530

    338. Counting Bits

--- countBits.py
+++ countBits.py
@@ -1,11 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-#         res=[];
-#         for i in range(n+1):
-#             res.append(bin(i).count('1'));
-#         return res;
-      
-        res=[0 for _ in range(n+1)];
-        for i in range(n+1):
-            res[i]=res[int(i/2)]+i%2;
+        res=[0] * (n+1);
+        offset=1
+        for i in range(1,n+1):
+            if offset*2==i:
+                offset=i
+            res[i]=res[i-offset]+1;
         return res; 
